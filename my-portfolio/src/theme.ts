d4f2f442c99af028deab27f32a0f2ccbd053bb17
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-02 09:49:30 -0300

    style: changed font typography

--- my-portfolio/src/theme.ts
+++ my-portfolio/src/theme.ts
@@ -10,7 +10,7 @@ let theme = createTheme({
     },
   },
   typography: {  /*fonte para o conteúdo da tag typography */
-    fontFamily: "Andale Mono, monospace",
+    fontFamily: "Bebas Neue",
 }
 });
 
