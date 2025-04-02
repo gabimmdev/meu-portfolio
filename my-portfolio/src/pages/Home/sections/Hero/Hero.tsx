b13059f0042ac4b4c8c210c81f932391e4a81248
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-01 23:52:24 -0300

    feat: changed  avatar position

--- my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
+++ my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -3,7 +3,6 @@ import Avatar from "../../../../assets/img/Avatar.jpeg"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
-
 const Hero = () => {
 
     const StyledHero = styled("div")(({theme})=>({
@@ -17,6 +16,7 @@ const Hero = () => {
         width: "80%",
         borderRadius: "50%",
         border: `2px solid ${theme.palette.secondary.contrastText}`,
+        zIndex: "5",
     }))
 
 
@@ -27,16 +27,16 @@ const Hero = () => {
                 <Grid container spacing={2}> {/* espaço dos containers */}
                     {/* xs = responsividade para telas pequenas | md = responsividade para telas médias */}
                     <Grid item xs={12} md={5}> {/* tamanho da tela que ocupa | nesse caso xs = 12 de 12 e md = 4 de 12 */}
-                        <Box position="relative">
-                        <Box position="relative" textAlign="center">
-                        <StyledImg src={Avatar} />
-                        </Box>
-                        </Box>
+                            <Box position="relative">
+                                <Box position="absolute" textAlign="center">
+                                <StyledImg src={Avatar} />
+                                </Box>
+                            </Box>
                     </Grid>
                     <Grid item xs={12} md={7}> 
                         {/* primary = tema cor padrão do material UI | vai ser mudado depois. */}
-                        <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Gabriela Gomes</Typography>
-                        <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Front End Developer</Typography>
+                        <Typography color="primary.contrastText" textAlign="center" pb={2} fontSize={70}>Gabriela Gomes</Typography>
+                        <Typography color="primary.contrastText" textAlign="center" fontSize={50}>I'm a Front-End Developer</Typography>
                         <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center"> {/* xs=12 deixa os botões um embaixo do outro |  md=4 deixa os botões um ao lado do outro */}
                                 <StyledButton>
@@ -50,7 +50,7 @@ const Hero = () => {
                                 <StyledButton>
                                 <EmailIcon /> {/* icone escolhido do material UI */}
                                     <Typography>
-                                    Contact me
+                                    Contact Me
                                     </Typography>
                                 </StyledButton>     
                             </Grid>
