91335aeef7156e895da0d5e07a05a117d5d98e74
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-02 23:39:37 -0300

    feat: import components

--- my-portfolio/src/pages/Home/Home.tsx
+++ my-portfolio/src/pages/Home/Home.tsx
@@ -1,11 +1,17 @@
 import NavBar from "../../components/NavBar/NavBar"
 import Hero from "./sections/Hero/Hero"
+import AboutSection from "./sections/About/About"
+import ProjectsSection from "./sections/Projects/Projects"
+import Footer from "../../components/Footer/Footer"
 
 const Home = () => {
     return (
       <>
       <NavBar/>
-        <Hero />
+      <Hero />
+      <AboutSection/>
+      <ProjectsSection/>
+      <Footer/>
       </>
     )
   }
