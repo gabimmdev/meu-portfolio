1333dfef45077f18fc54d967aba26a6b037278d0
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-02 21:52:47 -0300

    fix: Hero component UI

--- my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
+++ my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,85 +1,79 @@
-import { Box, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, Grid, Typography, styled } from "@mui/material"
 import Avatar from "../../../../assets/img/Avatar.jpeg"
 import DownloadIcon from '@mui/icons-material/Download';
-import EmailIcon from '@mui/icons-material/Email';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
-import { keyframes } from "@mui/system";
-import Matcha from "../../../../assets/img/matcha-latte.png"
+import Matcha from "../../../../assets/img/matcha-latte.png";
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(({theme})=>({
+    const StyledHero = styled("div")(({ theme }) => ({
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
         display: "flex",
         alignItems: "center",
+        [theme.breakpoints.up('xs')]: { // <= mobile
+            paddingTop: "100px",
+
+        },
+        [theme.breakpoints.up('md')]: { // >=mobile
+            paddingTop: "0",
+        }
     }))
 
-    const StyledImg = styled("img")(({theme})=>({
-        width: "80%",
+    const StyledImg = styled("img")(({ theme }) => ({
+        width: "75%",
         borderRadius: "50%",
-        border: `2px solid ${theme.palette.secondary.contrastText}`,
-        zIndex: "5",
+        border: `1px solid ${theme.palette.primary.contrastText}`
     }))
 
-    // Animação de flutuação para o ícone de chá
-  const floatAnimation = keyframes`
-  0% { transform: translateY(0); }
-  50% { transform: translateY(-8px); }
-  100% { transform: translateY(0); }
-`;
-
-const TeaIcon = styled("img")({
-  width: "18%",
-  position: "absolute",
-  bottom: "10%",
-  right: "10%",
-  animation: `${floatAnimation} 2s infinite ease-in-out`,
-});
-
+    const TeaIcon = styled("img")({
+        width: "18%",
+        position: "absolute",
+        bottom: "10%",
+        right: "10%",
+    });
 
     return (
-      <>
-        <StyledHero>
-            <Container maxWidth="lg"> {/* container de responsividade p/ tamanhos de tela maiores */}
-                <Grid container spacing={2}> {/* espaço dos containers */}
-                    {/* xs = responsividade para telas pequenas | md = responsividade para telas médias */}
-                    <Grid item xs={12} md={5}> {/* tamanho da tela que ocupa | nesse caso xs = 12 de 12 e md = 4 de 12 */}
+        <>
+            <StyledHero>
+                <Container maxWidth="lg">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={5}>
                             <Box position="relative">
-                                <Box position="absolute" textAlign="center">
-                                <StyledImg src={Avatar} />
-                                <TeaIcon src={Matcha} />
+                                <Box position="relative" textAlign="center">
+                                    <StyledImg src={Avatar} />
+                                    <TeaIcon src={Matcha}/>
                                 </Box>
                             </Box>
-                    </Grid>
-                    <Grid item xs={12} md={7}> 
-                        {/* primary = tema cor padrão do material UI | vai ser mudado depois. */}
-                        <Typography color="primary.contrastText" textAlign="center" pb={2} variant="h1">Gabriela Gomes</Typography>
-                        <Typography color="primary.contrastText" textAlign="center" variant="h2">I'm a Front-End Developer</Typography>
-                        <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
-                            <Grid item xs={12} md={4} display="flex" justifyContent="center"> {/* xs=12 deixa os botões um embaixo do outro |  md=4 deixa os botões um ao lado do outro */}
-                                <StyledButton>
-                                <DownloadIcon />  {/* icone escolhido do material UI */}
-                                <Typography>
-                                Download CV
-                                </Typography>
-                                </StyledButton>
+                        </Grid>
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Gabriela Gomes</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center" >I'm a Front End Developer</Typography>
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <StyledButton>
+                                        <DownloadIcon />
+                                        <Typography>
+                                            Download CV
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                    <StyledButton>
+                                        <MailOutlineIcon />
+                                        <Typography>
+                                            Contact me
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
-                                <EmailIcon /> {/* icone escolhido do material UI */}
-                                    <Typography>
-                                    Contact Me
-                                    </Typography>
-                                </StyledButton>     
-                            </Grid>
-                        </Grid>       
+                        </Grid>
                     </Grid>
-                    </Grid>
-            </Container>
-        </StyledHero>
-      </>
+                </Container>
+            </StyledHero>
+        </>
     )
-  }
+}
 
-  export default Hero
+export default Hero
