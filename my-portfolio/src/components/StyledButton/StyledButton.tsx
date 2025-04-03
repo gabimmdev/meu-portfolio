074b902ff66fa73ae95b4b194e3ec5a029f16c5c
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-02 23:39:08 -0300

    feat: onClick

--- my-portfolio/src/components/StyledButton/StyledButton.tsx
+++ my-portfolio/src/components/StyledButton/StyledButton.tsx
@@ -3,9 +3,10 @@ import { ReactNode } from "react"
 
 interface StyledButtonProps {
     children: ReactNode
+    onClick?: () => void;
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
+const StyledButton: React.FC<StyledButtonProps> = ({ children, onClick }) => {
 
     const StyledButton = styled("button")(({ theme }) => ({
         backgroundColor: theme.palette.secondary.light,
@@ -25,9 +26,7 @@ const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
 
     return (
         <>
-            <StyledButton>
-                {children}
-            </StyledButton>
+            <StyledButton onClick={onClick}>{children}</StyledButton>
         </>
     )
 }
